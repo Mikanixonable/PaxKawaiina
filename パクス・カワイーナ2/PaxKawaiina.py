6cefac770e9d1ffd050f40d6bf1adb7ebe084147
Mikanixonable <47976119+Mikanixonable@users.noreply.github.com>
2023-09-17 19:24:30 +0900

    x

---
+++ パクス・カワイーナ2/PaxKawaiina.py
@@ -0,0 +1,121 @@
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⢀⢀⠠⡠⠠⡠⡰⣒⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢌⢂⠢⡡⢑⠡⠪⡹⢮⢲⢱⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡔⡡⢘⠨⡂⢆⠢⡀⡯⣮⡪⢂⠕⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢐⢇⠧⣑⢄⠐⠈⠪⢢⡫⡟⠌⡂⡂⠢⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡸⡱⡡⢳⠠⠠⠀⡀⠕⢍⠀⠀⢐⠨⢐⢐⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠌⢎⡂⠀⠉⣂⡕⠅⠕⠅⢁⠀⠐⡨⢐⢐⠨⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠅⠅⢅⠢⠀⢕⢗⢎⢎⠌⢀⠀⠠⠀⢂⢂⠢⠨⡈⠄⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⢡⢑⢐⢅⢐⠡⡃⡂⡑⡌⠄⠂⠂⠀⠐⡐⡡⢁⠢⠐⡀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⢨⢪⢢⡣⠧⢑⢼⡐⡅⢎⢾⢜⢶⠅⠀⠀⢱⢐⠡⠨⢐⠀⠄⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠄⡇⣗⢵⢙⠈⣔⠅⡏⡪⠘⠨⣻⢮⡂⠀⠀⠨⡪⡊⠌⡀⠂⠀⠄⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣜⢮⡺⣜⡀⡢⡳⡅⢌⢆⠁⠀⡯⣗⡇⠀⠀⠀⢹⡪⡀⠄⠂⠀⠀⠄⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠠⡸⡪⢣⢙⠜⡰⡹⢸⢪⠡⠪⡄⠀⣯⣳⢣⠀⠀⠀⠐⡝⡆⡂⢂⠈⢀⠠⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠠⢱⡁⠆⣇⢕⢕⢜⠮⡨⠂⢕⣕⢌⠢⣗⢗⢽⡀⠀⠀⠀⢣⡳⡨⢐⠨⠀⠄⠐⡀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠠⣑⢵⢕⡼⣾⣺⢾⣗⢅⠕⡱⠘⡜⣌⡮⣯⡇⡎⣇⠀⠀⠀⠨⣪⠢⡂⢊⠌⢂⠡⠐⠀⠀
+# ⠀⠀⠀⠀⠀⠠⣑⡾⣝⡆⢟⢼⡺⡯⡯⣳⡐⣄⡧⡯⡗⡫⣷⣳⠱⡽⡀⠀⠀⠀⡣⡣⠊⠄⢌⠐⠌⠌⡂⠀
+# ⠀⠀⠀⠀⡠⠡⡢⡻⡮⡏⡢⢇⢋⠯⠯⡪⢏⠳⡽⠰⢈⢸⣗⣗⢕⢝⣆⠀⠀⠀⢪⢪⠨⠨⠠⠡⠡⠡⠨⡀
+# ⠀⠀⠀⡐⠄⢅⣗⢥⢪⣂⠄⡂⠄⠩⡈⠀⠁⠃⠅⢃⢢⠻⣺⡪⣎⢮⠳⠀⠀⠀⢂⠇⢌⠌⠌⠌⠌⠌⡂⡂
+# ⠀⠀⡐⡐⢡⡳⣕⢧⡣⣯⣻⢺⣪⠢⠂⠀⠀⠘⡽⡽⣕⢧⠀⠂⠂⠁⠀⠀⠀⠀⢐⠅⠅⠌⠌⠌⠌⡂⡂⡂
+# ⠀⢐⢐⢈⢲⢝⢮⡳⣕⣗⣗⢗⡗⢅⠀⠀⠀⠀⢹⣝⢮⣳⡀⠀⠀⠀⠀⠀⠀⠀⡐⠌⢜⠨⡊⠌⡂⡂⠢⠨
+# ⠀⢂⢂⠢⢣⢫⡳⣝⢮⣞⣮⡳⡝⠄⠀⠀⠀⠀⠐⡽⣕⡷⡅⠀⠀⠀⠀⠀⠀⠀⢌⢜⢜⢜⢌⢂⠢⠨⠨⠨
+# ⠈⠄⡂⢌⠪⡪⡪⡮⡳⣗⣗⢯⡇⡅⠀⠀⠀⠀⠀⢸⣗⢯⢯⠀⠀⠀⠀⠀⠀⠨⡐⡼⣕⢕⢕⠔⡡⠡⢁⠡
+# ⠄⠁⡂⠢⡑⢌⢺⡪⣏⡷⣝⢗⡗⡕⠀⠀⠀⠀⠀⠈⡮⣏⢯⡂⠀⠀⠀⠀⠀⡪⢰⢣⡣⡣⡣⡊⠄⠂⠀⠀
+# ⡂⡐⠌⡂⡊⠔⡵⡹⣜⣟⣮⡳⡑⠩⡀⠀⠀⠀⠀⠀⠹⣮⡳⣇⠀⠀⠀⠀⡨⡪⡪⡪⡪⡪⡪⢌⢪⠠⠁⠀
+# ⢂⠔⠡⡂⡪⢨⢪⢪⣺⣟⡮⡇⠀⠀⠂⡀⠀⠀⠀⠀⠀⠳⣯⣻⣆⠀⠀⢀⣗⢝⣜⢮⡪⣪⢪⡪⡂⠇⠁⠀
+# ⠐⡨⢸⢌⢆⢕⢕⣕⡯⣗⡯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣾⢽⣆⠀⠐⡕⢕⢕⢧⡫⡮⡳⡑⠁⠀⠀⠀
+# ⠀⠨⢪⢳⡱⡵⣕⠗⣯⣗⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣽⣺⠄⠀⠨⠈⢇⠧⡳⡹⠈⠀⠀⠀⠀⠀
+# ⠀⠀⠈⠑⢝⢜⢘⠀⡷⣯⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣞⣗⠀⠀⠈⠀⠅⠃⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠁⠂⠁⡯⣗⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢺⣝⠆⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⣯⠳⡅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡪⢯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⢮⡫⡪⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⢇⢇⢗⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# ⠀⠀⠀⠀⠀⠀⠀⠀⠹⡎⠗⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢕⢕⠳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+# Thy Pax Kawaiina come
+
+import json
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from tensorflow.keras.preprocessing.image import img_to_array
+import os
+import re
+import shutil
+import glob
+import pathlib
+import pyexiv2
+
+# path(str型)を入れると二要素の配列を返す。
+#一つ目にはjpgとpngのファイル名一覧の配列が入っている、
+#二つ目にはそれ以外のファイルとフォルダ一覧の配列が入っている
+def imgOrNotimg(dirname):
+    patternStr = '.+\.(jpg|png|jpeg|JPG|PNG)'
+    pattern = re.compile(patternStr)
+    imgs = []
+    noimgs = []
+    for item in os.listdir(dirname):
+        result = pattern.match(item)
+        # resultがNone以外=画像 なのでパスをリストに追加
+        if result:
+            imgs.append(dirname + '\\' + item)
+        else:
+            noimgs.append(dirname + '\\' + item)
+    i = 0
+    for item in imgs:
+        print(str(i) + ' : ' + item)
+    for item in noimgs:
+        print(str(i) + ' : ' + item)
+    return [imgs,noimgs]
+
+# signature読込
+with open('signature.json', 'r') as f:
+    signature = json.load(f)
+
+inputs = signature.get('inputs')
+outputs = signature.get('outputs')
+classes = signature.get('classes')
+
+# モデルの学習画像サイズを取得
+input_width, input_height = inputs['Image']['shape'][1:3]
+
+# model読込
+model = tf.saved_model.load('') #ここで時刻がプリントされる
+infer = model.signatures['serving_default']
+
+# 画像データ読込
+parent = str(pathlib.Path(os.getcwd()).parent)
+imgs,noimgs = imgOrNotimg(parent)
+
+for img_path in imgs:
+    #画像を配列に変換
+    imgPIL = Image.open(img_path)
+    imgPIL = imgPIL.convert('RGB')
+    imgPIL = imgPIL.resize((input_width, input_height))
+    x = img_to_array(imgPIL) / 255 #正規化
+    x = x[None, ...]
+
+    # 推論
+    predict = infer(tf.constant(x))
+    label = classes['Label']
+    varr = predict['Confidences'][0].numpy()
+    value = label[np.argmax(varr)] #"Not2D"か "KawaiiPictures"がはいる
+    if value == "Not2D":
+        with pyexiv2.Image(img_path) as img:
+            # data = img.read_exif()['Exif.Image.XPKeywords']
+            # if img.read_exif()['Exif.Image.HostComputer'] == 'iPhone 13 Pro':
+            try:
+                print(img.read_exif()['Exif.Image.Model'])
+                device = img.read_exif()['Exif.Image.Model']
+                os.makedirs(parent+'/'+ device, exist_ok=True)
+                shutil.move(img_path, parent+'/'+device)
+                print(device)
+            except KeyError:
+                os.makedirs(parent+'/'+ "Not2D", exist_ok=True)
+                shutil.move(img_path, parent+'/'+"Not2D")
+
+    else:
+        os.makedirs(parent+'/'+str(value), exist_ok=True)
+        shutil.move(img_path, parent+'/'+str(value))
+
+for path in noimgs:
+    if os.path.isfile(path):
+        os.makedirs(parent+'/sonota', exist_ok=True)
+        shutil.move(path, parent+'/sonota')
